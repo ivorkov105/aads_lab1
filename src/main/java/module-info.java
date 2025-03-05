434f17b723ba3e5e362ad59d88662064dc92da29
dvrk_rave <ivorkov105@gmail.com>
2025-03-05 19:52:16 +0700

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module aads.labs.lab1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens aads.labs.lab1 to javafx.fxml;
+    exports aads.labs.lab1;
+}
